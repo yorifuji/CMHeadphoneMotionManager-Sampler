b1585de864ad59942b3cc5dad039e564ed7f67b8
yorifuji <yorifuji@gmail.com>
2020-09-29 00:15:48 +0900

    start motion handler

--- CMHeadphoneMotionManager-Sampler/ViewController.swift
+++ CMHeadphoneMotionManager-Sampler/ViewController.swift
@@ -6,12 +6,36 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+
+    let hmm = CMHeadphoneMotionManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        if !hmm.isDeviceMotionAvailable {
+            print("current device does not supports the headphone motion manager.")
+            return
+        }
+
+//        if !hmm.isDeviceMotionActive {
+//            print("the headphone motion manager is not active.")
+//            return
+//        }
+
+//        hmm.startDeviceMotionUpdates()
+
+        hmm.startDeviceMotionUpdates(to: .main) { (motion, error) in
+            if let motion = motion {
+                print(motion)
+            }
+            if let error = error {
+                print(error)
+            }
+        }
     }
 
 
