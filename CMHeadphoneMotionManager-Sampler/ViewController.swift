f8dc4838b5d91437ab9cb1f3414698f3ee4c1bb6
yorifuji <yorifuji@gmail.com>
2020-09-28 23:34:23 +0900

    Initial Commit

---
+++ CMHeadphoneMotionManager-Sampler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CMHeadphoneMotionManager-Sampler
+//
+//  Created by yorifuji on 2020/09/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
