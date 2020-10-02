047841baa9c08042a9beb22ee8b1f3d80aa46eb9
yorifuji <yorifuji@gmail.com>
2020-10-02 18:04:26 +0900

    MotionWriter.swift

---
+++ CMHeadphoneMotionManager-Sampler/MotionWriter.swift
@@ -0,0 +1,75 @@
+//
+//  MotionWriter.swift
+//  CMHeadphoneMotionManager-Sampler
+//
+//  Created by yorifuji on 2020/10/03.
+//
+
+import Foundation
+import CoreMotion
+
+class MotionWriter {
+
+    var file: FileHandle?
+    var sample: Int = 0
+
+    func open(_ filePath: URL) {
+        do {
+            FileManager.default.createFile(atPath: filePath.path, contents: nil, attributes: nil)
+            let file = try FileHandle(forWritingTo: filePath)
+            var header = ""
+            header += "acceleration_x,"
+            header += "acceleration_y,"
+            header += "acceleration_z,"
+            header += "attitude_pitch,"
+            header += "attitude_roll,"
+            header += "attitude_yaw,"
+            header += "gravity_x,"
+            header += "gravity_y,"
+            header += "gravity_z,"
+            header += "quaternion_x,"
+            header += "quaternion_y,"
+            header += "quaternion_z,"
+            header += "quaternion_w,"
+            header += "rotation_x,"
+            header += "rotation_y,"
+            header += "rotation_z"
+            header += "\n"
+            file.write(header.data(using: .utf8)!)
+            self.file = file
+        } catch let error {
+            print(error)
+        }
+    }
+
+    func write(_ motion: CMDeviceMotion) {
+        guard let file = self.file else { return }
+        var text = ""
+        text += "\(motion.userAcceleration.x),"
+        text += "\(motion.userAcceleration.y),"
+        text += "\(motion.userAcceleration.z),"
+        text += "\(motion.attitude.pitch),"
+        text += "\(motion.attitude.roll),"
+        text += "\(motion.attitude.yaw),"
+        text += "\(motion.gravity.x),"
+        text += "\(motion.gravity.y),"
+        text += "\(motion.gravity.z),"
+        text += "\(motion.attitude.quaternion.x),"
+        text += "\(motion.attitude.quaternion.y),"
+        text += "\(motion.attitude.quaternion.z),"
+        text += "\(motion.attitude.quaternion.w),"
+        text += "\(motion.rotationRate.x),"
+        text += "\(motion.rotationRate.y),"
+        text += "\(motion.rotationRate.z)"
+        text += "\n"
+        file.write(text.data(using: .utf8)!)
+        sample += 1
+    }
+
+    func close() {
+        guard let file = self.file else { return }
+        file.closeFile()
+        print("\(sample) sample")
+        self.file = nil
+    }
+}
--- CMHeadphoneMotionManager-Sampler/ViewController.swift
+++ CMHeadphoneMotionManager-Sampler/ViewController.swift
@@ -11,6 +11,12 @@ import CoreMotion
 class ViewController: UIViewController {
 
     let hmm = CMHeadphoneMotionManager()
+    var writer: MotionWriter?
+    var begin: Date?
+
+    @IBOutlet weak var dirText: UITextField!
+    @IBOutlet weak var durationText: UITextField!
+    @IBOutlet weak var button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,26 +24,80 @@ class ViewController: UIViewController {
 
         if !hmm.isDeviceMotionAvailable {
             print("current device does not supports the headphone motion manager.")
-            return
         }
-
-//        if !hmm.isDeviceMotionActive {
-//            print("the headphone motion manager is not active.")
-//            return
-//        }
-
-//        hmm.startDeviceMotionUpdates()
-
+        // handle start device motion
         hmm.startDeviceMotionUpdates(to: .main) { (motion, error) in
             if let motion = motion {
-                print(motion)
+//                print(motion)
+                if let writer = self.writer {
+                    writer.write(motion)
+                    let duration = Int(self.durationText.text!)!
+                    if duration > 0 {
+                        let now = Date()
+                        if now.timeIntervalSince(self.begin!) > Double(duration) {
+                            self.stop()
+                            _ = self.start(self.dirText.text!)
+                        }
+                    }
+                }
             }
             if let error = error {
                 print(error)
             }
         }
+
     }
 
+    @IBAction func onButtonTap(_ sender: Any) {
+        if self.writer == nil && start(dirText.text!) {
+            button.setTitle("stop", for: .normal)
+        }
+        else {
+            stop()
+            button.setTitle("start", for: .normal)
+        }
+    }
+}
 
+extension ViewController {
+
+    func start(_ dirName: String) -> Bool {
+
+        // Document dir in app sandbox.
+        let document = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+
+        // create directory
+        do {
+            try FileManager.default.createDirectory(
+                at: document.appendingPathComponent(dirName, isDirectory: true),
+                withIntermediateDirectories: true,
+                attributes: nil)
+        } catch let error {
+            print(error)
+            return false
+        }
+
+        // generate file path
+        let formatter: DateFormatter = DateFormatter()
+        formatter.dateFormat = "yyyyMMdd_HHmmss"
+        let filename = formatter.string(from: Date()) + ".csv"
+        let fileUrl1 = document.appendingPathComponent(dirName, isDirectory: true)
+        let fileUrl = fileUrl1.appendingPathComponent(filename)
+        print(fileUrl.absoluteURL)
+
+        // setup writer
+        writer = MotionWriter()
+        writer?.open(fileUrl)
+        begin = Date()
+
+        return true
+    }
+
+    func stop() {
+        if let writer = self.writer {
+            writer.close()
+            self.writer = nil
+        }
+    }
 }
 
